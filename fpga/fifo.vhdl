a484e56f70e15dca972dd81663bc75616f0f4333
Petteri Aimonen <jpa@git.mail.kapsi.fi>
2012-11-17 23:28:17 +0200

    Faster FIFO

--- fpga/fifo.vhdl
+++ fpga/fifo.vhdl
@@ -11,7 +11,7 @@ use ieee.numeric_std.all;
 entity FIFO is
     generic (
         width_g: natural; -- Width (in bits) of the data items.
-        depth_g: natural  -- Depth of the buffer, as number of items.
+        depth_g: natural  -- Depth of the buffer, must be power of 2.
     );
     
     port (
@@ -21,7 +21,8 @@ entity FIFO is
         -- Data output port. Next data item is available on the
         -- output port when 'count' is not 0. When 'read' is
         -- high on clock edge, the item is removed from the FIFO
-        -- and next value is available on the port.
+        -- and next value is available on the port one clock cycle
+        -- later.
         data_out:   out std_logic_vector(width_g - 1 downto 0);
         read:       in std_logic;
 
@@ -33,80 +34,69 @@ entity FIFO is
         write:      in std_logic;
         
         -- Current number of items in FIFO.
+        -- Updates 1 clock cycle after write/read.
         count:      out std_logic_vector(15 downto 0)
     );
 end entity;
     
 architecture rtl of FIFO is
+    -- Function to calculate base-2 logarithm of the generic parameters
+    -- Result is rounded up to next integer value.
+    function log2(x : positive) return natural is 
+    begin
+            if x <= 1 then
+                    return 0;
+            else
+                    return log2(x / 2 + x mod 2) + 1;
+            end if;
+    end function log2;
+
+    -- Number of bits for the read/write position counters
+    constant counter_width_c : natural := log2(depth_g);
+
+    -- buffer_r is the large RAM array that is used as a ring buffer
     subtype FIFOEntry is std_logic_vector(width_g - 1 downto 0);
     type FIFOArray is array(depth_g - 1 downto 0) of FIFOEntry;
-    
-    -- buffer_r is the large RAM array that is used as a ring buffer
-    signal buffer_r: FIFOArray;
-    
-    signal data_out_r: std_logic_vector(width_g - 1 downto 0);
+    signal buffer_r:    FIFOArray;
     
     -- read_pos_r contains the index of the next item to read.
     -- write_pos_r contains the index of the next item to write.
-    signal read_pos_r: natural range 0 to depth_g - 1;
-    signal write_pos_r: natural range 0 to depth_g - 1;
+    -- When read_pos_r = write_pos_r, FIFO is empty.
+    -- When read_pos_r = write_pos_r + 1, FIFO is full.
+    signal read_pos_r:  unsigned(counter_width_c - 1 downto 0);
+    signal write_pos_r: unsigned(counter_width_c - 1 downto 0);
     
-    -- count_r contains the number of items in FIFO currently.
-    signal count_r: natural range 0 to depth_g;
+    -- Registers for output signals
+    signal data_out_r:  std_logic_vector(width_g - 1 downto 0);
+    signal count_r:     unsigned(counter_width_c - 1 downto 0);
+    
+    signal next_write_pos_r: unsigned(counter_width_c - 1 downto 0);
 begin
     data_out <= data_out_r;
-    count <= std_logic_vector(to_unsigned(count_r, 16));
+    count(counter_width_c - 1 downto 0) <= std_logic_vector(count_r);
+    count(15 downto counter_width_c) <= (others => '0');
     
     process(clk, rst_n)
-        -- Using variables to store the read / write status.
-        variable read_v: boolean;
-        variable write_v: boolean;
-        variable read_pos_v: natural range 0 to depth_g - 1;
     begin
         if rst_n = '0' then
-            read_pos_r <= 0;
-            write_pos_r <= 0;
-            count_r <= 0;
+            read_pos_r <= (others => '0');
+            write_pos_r <= (others => '0');
+            count_r <= (others => '0');
             data_out_r <= (others => '0');
+            next_write_pos_r <= to_unsigned(1, counter_width_c);
         elsif rising_edge(clk) then
-            read_v := (read = '1') and (count_r /= 0);
-            write_v := (write = '1') and (count_r /= depth_g);
-        
-            read_pos_v := read_pos_r;
-        
-            if read_v then
-                if read_pos_r = depth_g - 1 then
-                    read_pos_v := 0;
-                else
-                    read_pos_v := read_pos_r + 1;
-                end if;
-                
-                if not write_v then
-                    count_r <= count_r - 1;
-                end if;
+            count_r <= write_pos_r - read_pos_r;
+            data_out_r <= buffer_r(to_integer(read_pos_r));
+            
+            if read = '1' and read_pos_r /= write_pos_r then
+                read_pos_r <= read_pos_r + 1;
             end if;
             
-            if write_v then
-                buffer_r(write_pos_r) <= data_in;
-                
-                if write_pos_r = depth_g - 1 then
-                    write_pos_r <= 0;
-                else
-                    write_pos_r <= write_pos_r + 1;
-                end if;
-                
-                if not read_v then
-                    count_r <= count_r + 1;
-                end if;
+            if write = '1' and next_write_pos_r /= read_pos_r then
+                buffer_r(to_integer(write_pos_r)) <= data_in;
+                write_pos_r <= next_write_pos_r;
+                next_write_pos_r <= next_write_pos_r + 1;
             end if;
-            
-            if read_pos_v = write_pos_r and write_v then
-                data_out_r <= data_in;
-            else
-                data_out_r <= buffer_r(read_pos_v);
-            end if;
-            
-            read_pos_r <= read_pos_v;
         end if;
     end process;
 end architecture;
--- fpga/tb_fifo.vhdl
+++ fpga/tb_fifo.vhdl
@@ -38,34 +38,52 @@ begin
         
         assert unsigned(count) = 0 report "FIFO should be empty after reset";
         
-        -- Write until fifo is full
-        for i in 1 to fifo_depth_g loop
+        -- Write slowly
+        for i in 1 to 10 loop
             data_in <= std_logic_vector(to_unsigned(i, fifo_width_g));
             write <= '1';
             clock;
+            write <= '0';
+            clock;
             assert unsigned(count) = i report "FIFO count is wrong in write";
         end loop;
+        
+        -- Write every clock cycle
+        write <= '1';
+        for i in 11 to fifo_depth_g - 1 loop
+            data_in <= std_logic_vector(to_unsigned(i, fifo_width_g));
+            clock;
+        end loop;
         write <= '0';
+        
+        clock;
+        assert unsigned(count) = fifo_depth_g - 1 report "FIFO should be full";
         
         -- Do a few dummy writes to test writing when full. These should
         -- be ignored by the FIFO.
         write <= '1';
         clock; clock; clock;
         write <= '0';
-        assert unsigned(count) = fifo_depth_g
+        assert unsigned(count) = fifo_depth_g - 1
             report "FIFO should ignore writes when full";
        
-        for i in 1 to fifo_depth_g loop
+        for i in 1 to fifo_depth_g - 1 loop
             output := to_integer(unsigned(data_out));
             assert output = i
                 report "FIFO output is wrong: " & natural'Image(output)
                 & " should be " & natural'Image(i);
+            
             read <= '1';
             clock;
-            assert unsigned(count) = fifo_depth_g - i
+            read <= '0';
+            clock;
+            
+            assert unsigned(count) = fifo_depth_g - 1 - i
                 report "FIFO count is wrong in read";
         end loop;
         read <= '0';
+        
+        clock;
         
         assert unsigned(count) = 0 report "FIFO should be empty";
         
